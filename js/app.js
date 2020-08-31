15112420127c674a04dae91460cb9e93ec82abff
CarissaAyo <ajaoyussufayo@gmail.com>
2020-08-31 14:04:53 +0100

    javascript for the inputs

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let inputs = document.querySelectorAll('input');
+
+inputs.forEach((input)=>{
+   addEventListener('keyup',(e)=>{
+       console.log(e.target.value)
+   })
+}
+)
+validate (input){
+    console.log()
+
+}
