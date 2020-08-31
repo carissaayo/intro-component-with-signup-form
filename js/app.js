b8be8a803950b7f31d9b0aacf2875558a071e8c1
CarissaAyo <ajaoyussufayo@gmail.com>
2020-08-31 15:34:09 +0100

    the validaing function, regex patterns and events have been created

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 let inputs = document.querySelectorAll('input');
 
+let patterns ={
+    FirstName: /^[a-z]{4,}$/i,
+    LastName: /^[a-z]{4,}$/i,
+    EmailAddress: /^([a-z\d-\.]+)@([a-z\d-]+)\.([a-z]{2,5})(\.{2,5})?$/i,
+    password: /^[a-z\d@%-&*]{7,15}$/,
+}
 inputs.forEach((input)=>{
-   addEventListener('keyup',(e)=>{
-       console.log(e.target.value)
+   input.addEventListener('keyup',(e)=>{
+       validate(e.target,patterns[e.target.attributes.name.value])
    })
 }
 )
-validate (input){
-    console.log()
+function validate(field,regex){
+    if(regex.test(field.value)){
+    field.className = 'valid'     
+    }else{
+        field.className ='invalid'
+    }
 
 }
+
+
